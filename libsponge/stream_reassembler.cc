799b5ac5bd36efe7d444eac3b179e5acd41d50af
sunhuiquan <sunhuiquan@yeah.net>
2021-03-18 21:37:06 +0800

    add finish

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -7,8 +7,8 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// template <typename... Targs>
+// void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -17,10 +17,83 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof)
+{
+    if (eof == true)
+    {
+        _eof = true;
+    }
+
+    auto a = _slices.insert(pair<uint64_t, std::string>(index, data));
+    auto b = _slices.lower_bound(pair<uint64_t, std::string>(index, data));
+    if (b != _slices.end() && b->first != index)
+    {
+        _slices.erase(a.first);
+    }
+    else
+    {
+        _unassembled_bytes += a.first->second.size();
+    }
+
+    auto it = a.first;
+    while (b != _slices.end())
+    {
+        if (it->first + it->second.size() == b->first)
+        {
+            auto aa = _slices.insert(pair<uint64_t, std::string>(it->first, it->second + b->second));
+            _slices.erase(it);
+            _slices.erase(b);
+            b = _slices.lower_bound(*(aa.first));
+            it = aa.first;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    b = _slices.upper_bound(*it);
+    while (1)
+    {
+        if (b->first + b->second.size() == it->first)
+        {
+            auto aa = _slices.insert(pair<uint64_t, std::string>(b->first, b->second + it->second));
+            _slices.erase(b);
+            _slices.erase(it);
+            b = _slices.upper_bound(*(aa.first));
+            it = aa.first;
+        }
+        else
+        {
+            break;
+        }
+        if (b == _slices.begin())
+        {
+            break;
+        }
+    }
+
+    if (_eof == true)
+    {
+        if (_slices.size() == 1)
+        {
+            _output.write(_slices.begin()->second);
+            _unassembled_bytes -= _slices.begin()->second.size();
+            _slices.clear();
+        }
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const
+{
+    return _unassembled_bytes;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const
+{
+    if (unassembled_bytes() == 0)
+    {
+        return true;
+    }
+    return false;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,17 +5,22 @@
 
 #include <cstdint>
 #include <string>
+#include <set>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
-class StreamReassembler {
-  private:
+class StreamReassembler
+{
+private:
     // Your code here -- add private members as necessary.
+    std::set<pair<uint64_t, std::string>> _slices = {};
+    size_t _unassembled_bytes = 0;
+    bool _eof = false;
 
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    ByteStream _output; //!< The reassembled in-order byte stream
+    size_t _capacity;   //!< The maximum number of bytes
 
-  public:
+public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
     //! and those that have not yet been reassembled.
@@ -50,4 +55,4 @@ class StreamReassembler {
     bool empty() const;
 };
 
-#endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
+#endif // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH

138b073258ba5f4f95873012ad3a14f620a735db
sunhuiquan <sunhuiquan@yeah.net>
2021-03-24 22:05:54 +0800

    add .gitignore

--- build/CMakeFiles/check_lab0.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/check_lab0"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/check_lab0.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/apps/CMakeFiles/stream_copy.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libstream_copy.a"
-)

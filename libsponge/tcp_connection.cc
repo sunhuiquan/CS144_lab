28beccb1ab8d6121fb245fbbfcd00e4bbdf087e7
sunhuiquan <sunhuiquan@yeah.net>
2021-03-25 08:55:56 +0800

    fix a bug

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -82,10 +82,22 @@ void TCPConnection::segment_received(const TCPSegment &seg)
     }
 
     _receiver.segment_received(seg);
-    _sender.ack_received(seg.header().ackno, seg.header().win);
+    // _sender.ack_received(seg.header().ackno, seg.header().win);
+
+    //after establising and receive a bad ack, you should send empty(the exists one)
+    if (!_sender.ack_received(seg.header().ackno, seg.header().win))
+    {
+        if (_sender.next_seqno_absolute() > 0 && seg.header().ack)
+        {
+            if (_receiver.ackno().has_value() && _sender.segments_out().empty() && !seg.length_in_sequence_space())
+            {
+                _sender.send_empty_segment();
+            }
+        }
+    }
 
     // empty segment will add ACK in send_segments function
-    if (_sender.stream_in().buffer_empty() && seg.length_in_sequence_space())
+    if (_sender.segments_out().empty() && seg.length_in_sequence_space())
     {
         _sender.send_empty_segment();
     }
